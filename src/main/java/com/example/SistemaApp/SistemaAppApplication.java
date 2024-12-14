0720ba50461d6d3392a59b63656d05d68e2cfae1
rafaRafoso <rafaelcsadm@hotmail.com>
2024-12-14 18:03:50 -0300

    iniciado a base do projeto

---
+++ src/main/java/com/example/SistemaApp/SistemaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SistemaApp/SistemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
